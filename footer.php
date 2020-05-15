ecb9b094f21bd235bc540b951f74571bf5399faf
Ilan93420 <64796623+Ilan93420@users.noreply.github.com>
2020-05-15 15:50:50 +0200

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<div id="footer">
-    <span>© Copyright 2020 - Tous droits réservés</span>
-    <a href="#" class="fa fa-facebook"></a>
-    <a href="#" class="fa fa-twitter"></a>
-    <a href="#" class="fa fa-linkedin"></a>
-    <a href="#" class="fa fa-youtube"></a>
-    <a href="https://www.instagram.com/iabitbol/?hl=fr" class="fa fa-instagram"></a>
-</div>
